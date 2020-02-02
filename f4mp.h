6dcf53079c19a7021e3ef8019c706fea2e61429e
cokwa <rhrh1363@gmail.com>
2020-02-02 23:56:12 +0900

    Add player data system and rotatoin

--- f4mp.h
+++ f4mp.h
@@ -6,6 +6,17 @@
 
 #include <memory>
 #include <vector>
+#include <unordered_map>
+
+struct PlayerData
+{
+	std::unordered_map<std::string, Float32> numbers;
+	
+	PlayerData()
+	{
+
+	}
+};
 
 class F4MP
 {
@@ -20,7 +31,7 @@ public:
 		return *instance;
 	}
 
-	F4MP() : ctx{}, port(0), playerEntityID((UInt32)-1), papyrus(nullptr)
+	F4MP() : ctx{}, currentEvent(nullptr), port(0), playerEntityID((UInt32)-1), papyrus(nullptr)
 	{
 		ctx.mode = LIBRG_MODE_CLIENT;
 
@@ -29,8 +40,13 @@ public:
 		librg_event_add(&ctx, LIBRG_CONNECTION_REQUEST, OnConnectRequest);
 		librg_event_add(&ctx, LIBRG_CONNECTION_ACCEPT, OnConnectAccept);
 		librg_event_add(&ctx, LIBRG_CONNECTION_REFUSE, OnConnectRefuse);
+		librg_event_add(&ctx, LIBRG_CONNECTION_DISCONNECT, OnDisonnect);
 
 		librg_event_add(&ctx, LIBRG_ENTITY_CREATE, OnEntityCreate);
+		librg_event_add(&ctx, LIBRG_ENTITY_UPDATE, OnEntityUpdate);
+		librg_event_add(&ctx, LIBRG_ENTITY_REMOVE, OnEntityRemove);
+
+		librg_event_add(&ctx, LIBRG_CLIENT_STREAMER_UPDATE, OnClientUpdate);
 	}
 
 	virtual ~F4MP()
@@ -47,7 +63,7 @@ public:
 				vm->RegisterFunction(new NativeFunction2<StaticFunctionTag, bool, BSFixedString, SInt32>("Connect", "F4MP", Connect, vm));
 				vm->RegisterFunction(new NativeFunction0<StaticFunctionTag, bool>("Disconnect", "F4MP", Disconnect, vm));
 				vm->RegisterFunction(new NativeFunction0<StaticFunctionTag, void>("Tick", "F4MP", Tick, vm));
-				vm->SetFunctionFlags("F4MP", "Tick", IFunction::kFunctionFlag_NoWait);
+				//vm->SetFunctionFlags("F4MP", "Tick", IFunction::kFunctionFlag_NoWait);
 
 				vm->RegisterFunction(new NativeFunction0<StaticFunctionTag, UInt32>("GetPlayerEntityID", "F4MP", GetPlayerEntityID, vm));
 
@@ -55,6 +71,13 @@ public:
 				vm->RegisterFunction(new NativeFunction1<StaticFunctionTag, VMArray<Float32>, UInt32>("GetEntityPosition", "F4MP", GetEntityPosition, vm));
 				vm->RegisterFunction(new NativeFunction4<StaticFunctionTag, void, UInt32, Float32, Float32, Float32>("SetEntityPosition", "F4MP", SetEntityPosition, vm));
 				vm->SetFunctionFlags("F4MP", "SetEntityPosition", IFunction::kFunctionFlag_NoWait);
+
+				vm->RegisterFunction(new NativeFunction1<StaticFunctionTag, void, Float32>("WriteNumber", "F4MP", WriteNumber, vm));
+				vm->RegisterFunction(new NativeFunction0<StaticFunctionTag, Float32>("ReadNumber", "F4MP", ReadNumber, vm));
+				//vm->SetFunctionFlags("F4MP", "WriteNumber", IFunction::kFunctionFlag_NoWait);
+
+				vm->RegisterFunction(new NativeFunction3<StaticFunctionTag, void, UInt32, BSFixedString, Float32>("SetEntityNumber", "F4MP", SetEntityNumber, vm));
+				vm->RegisterFunction(new NativeFunction2<StaticFunctionTag, Float32, UInt32, BSFixedString>("GetEntityNumber", "F4MP", GetEntityNumber, vm));
 
 				return true;
 			}))
@@ -74,6 +97,7 @@ private:
 	UInt32 playerEntityID;
 
 	librg_ctx ctx;
+	librg_event* currentEvent;
 
 	F4SEPapyrusInterface* papyrus;
 
@@ -89,6 +113,8 @@ private:
 		_MESSAGE("successfully connected to %s:%d", self.address.c_str(), self.port);
 
 		self.playerEntityID = event->entity->id;
+
+		event->entity->user_data = new PlayerData();
 	}
 
 	static void OnConnectRefuse(librg_event* event)
@@ -97,10 +123,26 @@ private:
 		_ERROR("failed to connect to %s:%d!", self.address.c_str(), self.port);
 	}
 
+	static void OnDisonnect(librg_event* event)
+	{
+		PlayerData* data = (PlayerData*)event->entity->user_data;
+		if (data)
+		{
+			delete data;
+			event->entity->user_data = nullptr;
+		}
+	}
+
+	//TODO: abstract event handlers as they're basically the same
+
 	static void OnEntityCreate(librg_event* event)
 	{
+		event->entity->user_data = new PlayerData();
+
 		F4MP& self = GetInstance();
 		_MESSAGE("entity with ID '%d' has created", event->entity->id);
+
+		self.currentEvent = event;
 
 		self.papyrus->GetExternalEventRegistrations("OnEntityCreate", event, [](UInt64 handle, const char* scriptName, const char* callbackName, void* dataPtr)
 			{
@@ -109,6 +151,83 @@ private:
 				UInt32 id = event->entity->id;
 				SendPapyrusEvent1<UInt32>(handle, scriptName, callbackName, id);
 			});
+
+		self.currentEvent = nullptr;
+	}
+
+	static void OnEntityUpdate(librg_event* event)
+	{
+		F4MP& self = GetInstance();
+
+		self.currentEvent = event;
+
+		self.papyrus->GetExternalEventRegistrations("OnEntityUpdate", event, [](UInt64 handle, const char* scriptName, const char* callbackName, void* dataPtr)
+			{
+				librg_event* event = static_cast<librg_event*>(dataPtr);
+
+				UInt32 id = event->entity->id;
+				SendPapyrusEvent1<UInt32>(handle, scriptName, callbackName, id);
+			});
+
+		self.currentEvent = nullptr;
+
+		PlayerData* data = (PlayerData*)event->entity->user_data;
+		if (!data)
+		{
+			return;
+		}
+
+		data->numbers["angleX"] = librg_data_rf32(event->data);
+		data->numbers["angleY"] = librg_data_rf32(event->data);
+		data->numbers["angleZ"] = librg_data_rf32(event->data);
+	}
+
+	static void OnEntityRemove(librg_event* event)
+	{
+		PlayerData* data = (PlayerData*)event->entity->user_data;
+		if (data)
+		{
+			delete data;
+			event->entity->user_data = nullptr;
+		}
+
+		F4MP& self = GetInstance();
+		_MESSAGE("entity with ID '%d' has removed", event->entity->id);
+
+		self.papyrus->GetExternalEventRegistrations("OnEntityRemove", event, [](UInt64 handle, const char* scriptName, const char* callbackName, void* dataPtr)
+			{
+				librg_event* event = static_cast<librg_event*>(dataPtr);
+
+				UInt32 id = event->entity->id;
+				SendPapyrusEvent1<UInt32>(handle, scriptName, callbackName, id);
+			});
+	}
+
+	static void OnClientUpdate(librg_event* event)
+	{
+		F4MP& self = GetInstance();
+
+		self.currentEvent = event;
+
+		self.papyrus->GetExternalEventRegistrations("OnClientUpdate", event, [](UInt64 handle, const char* scriptName, const char* callbackName, void* dataPtr)
+			{
+				librg_event* event = static_cast<librg_event*>(dataPtr);
+
+				UInt32 id = event->entity->id;
+				SendPapyrusEvent1<UInt32>(handle, scriptName, callbackName, id);
+			});
+
+		self.currentEvent = nullptr;
+
+		PlayerData* data = (PlayerData*)event->entity->user_data;
+		if (!data)
+		{
+			return;
+		}
+
+		librg_data_wf32(event->data, data->numbers["angleX"]);
+		librg_data_wf32(event->data, data->numbers["angleY"]);
+		librg_data_wf32(event->data, data->numbers["angleZ"]);
 	}
 
 	static bool Connect(StaticFunctionTag* base, BSFixedString address, SInt32 port)
@@ -132,7 +251,7 @@ private:
 	{
 		F4MP& self = GetInstance();
 		librg_network_stop(&self.ctx);
-
+		
 		_MESSAGE("successfully ended the network connection");
 		return true;
 	}
@@ -185,5 +304,71 @@ private:
 		entity->position.x = x;
 		entity->position.y = y;
 		entity->position.z = z;
+	}
+
+	//TODO: exception for the event not being the proper one
+
+	static void WriteNumber(StaticFunctionTag* base, Float32 number)
+	{
+		F4MP& self = GetInstance();
+		if (!self.currentEvent || self.currentEvent->id != LIBRG_CLIENT_STREAMER_UPDATE)
+		{
+			_ERROR("id of %p is not LIBRG_CLIENT_STREAMER_UPDATE", self.currentEvent);
+			return;
+		}
+
+		librg_data_wf32(self.currentEvent->data, number);
+	}
+
+	static void WriteString(StaticFunctionTag* base, BSFixedString string)
+	{
+		
+	}
+
+	static Float32 ReadNumber(StaticFunctionTag* base)
+	{
+		F4MP& self = GetInstance();
+		if (!self.currentEvent || self.currentEvent->id != LIBRG_ENTITY_UPDATE)
+		{
+			_ERROR("id of %p is not LIBRG_ENTITY_UPDATE", self.currentEvent);
+			return 0.f;
+		}
+
+		return librg_data_rf32(self.currentEvent->data);
+	}
+
+	static BSFixedString ReadString(StaticFunctionTag* base)
+	{
+		return BSFixedString();
+	}
+
+	static void SetEntityNumber(StaticFunctionTag* base, UInt32 entityID, BSFixedString name, Float32 value)
+	{
+		F4MP& self = GetInstance();
+
+		librg_entity* entity = librg_entity_fetch(&self.ctx, entityID);
+		if (!entity)
+		{
+			_ERROR("no entity with ID '%d'!", entityID);
+			return;
+		}
+
+		PlayerData* data = (PlayerData*)entity->user_data;
+		data->numbers[name.c_str()] = value;
+	}
+
+	static Float32 GetEntityNumber(StaticFunctionTag* base, UInt32 entityID, BSFixedString name)
+	{
+		F4MP& self = GetInstance();
+
+		librg_entity* entity = librg_entity_fetch(&self.ctx, entityID);
+		if (!entity)
+		{
+			_ERROR("no entity with ID '%d'!", entityID);
+			return 0.f;
+		}
+
+		PlayerData* data = (PlayerData*)entity->user_data;
+		return data->numbers[name.c_str()];
 	}
 };
--- scripts/F4MP.psc
+++ scripts/F4MP.psc
@@ -9,3 +9,9 @@ int Function GetPlayerEntityID() global native
 bool Function IsEntityValid(int entityID) global native
 float[] Function GetEntityPosition(int entityID) global native
 Function SetEntityPosition(int entityID, float x, float y, float z) global native
+
+Function WriteNumber(float number) global native
+float Function ReadNumber() global native
+
+Function SetEntityNumber(int entityID, string name, float value) global native
+float Function GetEntityNumber(int entityID, string name) global native
