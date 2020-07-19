f5b87bec932771e2682c495329077dadafa568dc
Genetical <18619788+Genetical@users.noreply.github.com>
2020-07-19 09:58:44 +0100

    Update classes.py

--- server/F4MP/classes.py
+++ server/F4MP/classes.py
@@ -5,8 +5,20 @@ class Item:
         self.type = item_type
 
 
-class Attributes:
-    def __init__(self, strength, perception, endurance, charisma, intelligence, agility, luck, perks=None):
+class Inventory:
+    def __init__(self, items=None, equipped=None):
+        """Represents the inventory of a player. However is not tied to any specific player.
+        Args:
+            items (List[Item]): List of items a player has in its inventory
+            equipped (List[Item]): List of items player has equipped. Must be a subset of items.
+        """
+        assert set(equipped).issubset(set(items))
+        self.items = items
+        self.equipped = equipped
+
+
+class SPECIAL:
+    def __init__(self, strength, perception, endurance, charisma, intelligence, agility, luck):
         self.strength = strength
         self.perception = perception
         self.endurance = endurance
@@ -14,31 +26,43 @@ class Attributes:
         self.intelligence = intelligence
         self.agility = agility
         self.luck = luck
-        self.perks = perks
 
 
-class Status:
-    def __init__(self, hp: int, ap: int, max_hp, max_ap):
-        assert hp <= max_hp and ap <= max_ap  # Probably not neccesary, could make HP a decimal between 0 and 1
+class Player:
+    def __init__(self, id, position, hp, ap, special, inventory):
+        """
+
+        Args:
+            id (int): Server Generated ID for the player instance
+            position(Tuple[float, float, float]): 3D Vector
+            hp(float):
+            ap(float):
+            special(SPECIAL):
+            inventory(Inventory):
+        """
+        self.id = id
+        self.position = position
         self.hp = hp
         self.ap = ap
-        self.max_hp = max_hp
-        self.max_ap = max_ap
+        self.special = special
+        self.inventory = inventory
 
 
 class Connection:
-    def __init__(self, address):
+    def __init__(self, address, player: Player = None):
         self.address = address
-        ...  # More here probably
+        self.player = player
 
 
 class User:
-    def __init__(self, user_id, username, position, connection, status, attributes, inventory):
-        self.id = user_id
+    def __init__(self, id, username, connection):
+        self.id = id
         self.username = username
-        self.position = position
         self.connection = connection
 
-        self.status = status
-        self.attributes = attributes
-        self.inventory = inventory
+
+class Event:
+    def __init__(self, type, user, player: Player = None):
+        self.type = type
+        self.user = user
+        self.player = player
