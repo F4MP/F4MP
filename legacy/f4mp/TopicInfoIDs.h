7470e5528308bd545deb4333db0aec204c8db9cd
Craig Lennox <blipvert@blipvertica.com>
2020-10-21 13:11:11 -0400

    Removed legacy code, now on separate branch.

--- legacy/f4mp/TopicInfoIDs.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-namespace f4mp
-{
-	extern UInt32 topicInfoIDs[68227];
-}
--- legacy/f4mp/common.cpp
+++
@@ -1,6 +0,0 @@
-#include "common.h"
-
-u64 f4mp::GetUniqueFormID(u32 ownerEntityID, u32 entityFormID)
-{
-	return ((u64)ownerEntityID << 32) | entityFormID;
-}
